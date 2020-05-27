ba141f143cd1435e0879b65684c49ac3f93dd5b5
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-05-27 09:45:28 -0300

    show and destroy methods

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -15,14 +15,25 @@ class Api::V1::RecipesController < ApplicationController
   end
 
   def show
+    if find_recipe
+      render json: recipe
+    else
+      render json: recipe.errors
+    end
   end
 
   def destroy
+    recipe&.destroy
+    render json: { message: 'Recipe deleted!' }
   end
 
   private
    
   def recipe_params
     params.permit(:name, :ingredients, :instruction, :image)
+  end
+
+  def find_recipe
+    @recipe ||= Recipe.find(params[:id])
   end
 end
