f7e6e889540f59e7d16430a6fb1e44f31a144e58
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-05-27 10:04:16 -0300

    create recipes component and fetching data

---
+++ app/javascript/components/Recipes.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Recipes extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            recipes: []
+        };
+    }
+
+    componentDidMount(){
+        const url = "api/v1/recipes/index";
+
+        fetch(url)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error("Network response was not ok");
+            })
+            .then(response => this.setState({ recipes: response }))
+            .catch(() => this.props.history.push("/"));
+    }
+}
+
+export default Recipes;
