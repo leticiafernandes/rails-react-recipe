230d3ed84e3f92fa592933b2aa0fa89762e819ef
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-05-27 20:43:59 -0300

    render method and request method

--- app/javascript/components/NewRecipe.jsx
+++ app/javascript/components/NewRecipe.jsx
@@ -13,6 +13,7 @@ class NewRecipe extends React.Component {
         };
 
         this.onChange = this.onChange.bind(this);
+        this.request = this.request.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
     }
 
@@ -57,7 +58,64 @@ class NewRecipe extends React.Component {
             instruction
         };
 
-        request(url, body);
+        this.request(url, body);
+    }
+
+    render() {
+        return (
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-sm-12 col-lg-6 offset-lg-3">
+                        <h1 className="font-weight-normal mb-5">
+                        Add a new recipe to our awesome recipe collection.
+                        </h1>
+                        <form onSubmit={this.onSubmit}>
+                            <div className="form-group">
+                                <label htmlFor="recipeName">Recipe name</label>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    id="recipeName"
+                                    className="form-control"
+                                    required
+                                    onChange={this.onChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="recipeIngredients">Ingredients</label>
+                                <input
+                                    type="text"
+                                    name="ingredients"
+                                    id="recipeIngredients"
+                                    className="form-control"
+                                    required
+                                    onChange={this.onChange}
+                                />
+                                <small id="ingredientsHelp" className="form-text text-muted">
+                                    Separate each ingredient with a comma.
+                                </small>
+                            </div>
+                            <label htmlFor="instruction">Preparation Instructions</label>
+                            <textarea
+                                className="form-control"
+                                id="instruction"
+                                name="instruction"
+                                rows="5"
+                                required
+                                onChange={this.onChange}
+                            />
+                            <button type="submit" className="btn custom-button mt-3">
+                                Create Recipe
+                            </button>
+                            <Link to="/recipes" className="btn btn-link mt-3">
+                                Back to recipes
+                            </Link>
+                        </form>
+                    </div>
+                </div>
+            </div>
+ 
+        );
     }
 }
 
