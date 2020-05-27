f395c013110c20457c299c477e4792461f662822
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-05-27 20:21:12 -0300

    initalize new recipe component

---
+++ app/javascript/components/NewRecipe.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class NewRecipe extends React.Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            ingredients: "",
+            instruction: "",
+        };
+        //refactor to use hooks
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.stripHtmlEntities = this.stripHtmlEntities.bind(this);
+    }
+}
+
+export default NewRecipe;
